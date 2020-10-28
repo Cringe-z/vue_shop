a537c23bbde7fa606be457f94c85a5d024c94c02
HenryChou <zhouhengrui@foxmail.com>
2020-10-29 00:30:27 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-button type="info" @click="logout">退出</el-button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.removeItem('token');
+      this.$router.push('/login');
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
