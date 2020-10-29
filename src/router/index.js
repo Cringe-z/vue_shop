202cbcba619c28bae8802b0326cf6dcb7feec732
HenryChou <zhouhengrui@foxmail.com>
2020-10-29 23:46:07 +0800

    完成用户列表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,40 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Login from '../components/Login.vue';
+import Home from '../components/Home.vue';
+import Welcome from '../components/Welcome.vue';
+import Users from '../components/user/Users.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home },
-]
+  {
+    path: '/home',
+    component: Home,
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users },
+    ],
+    redirect: '/welcome',
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
 //挂载路由导航守卫
 router.beforeEach((to, from, next) => {
   // to 将要访问的路径
   // from 从哪个路径跳转而来
   // next 函数，next() 放行，next('/login') 强制跳转
-  if(to.path === '/login') return next();
+  if (to.path === '/login') return next();
   //获取token
   const tokenStr = window.sessionStorage.getItem('token');
-  if(!tokenStr) return next('/login');
+  if (!tokenStr) return next('/login');
   next();
-})
+});
 
-export default router
+export default router;
