2397a55f926545fce36b98c951b8a0ccac385c4c
HenryChou <zhouhengrui@foxmail.com>
2020-10-31 22:50:39 +0800

    完成了分类功能的开发

--- src/main.js
+++ src/main.js
@@ -1,25 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './plugins/element.js'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import './plugins/element.js';
 //导入字体图标
-import './assets/fonts/iconfont.css'
+import './assets/fonts/iconfont.css';
 //导入全局样式表
-import './assets/css/global.css'
+import './assets/css/global.css';
+import ZkTable from 'vue-table-with-tree-grid';
 
-import axios from 'axios'
+import axios from 'axios';
 //配置请求的根路径
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
-axios.interceptors.request.use(config => {
-  config.headers.Authorization = window.sessionStorage.getItem('token')
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/';
+axios.interceptors.request.use((config) => {
+  config.headers.Authorization = window.sessionStorage.getItem('token');
   //必须return config
-  return config
-})
-Vue.prototype.$http = axios
+  return config;
+});
+Vue.prototype.$http = axios;
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.component('tree-table', ZkTable);
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
